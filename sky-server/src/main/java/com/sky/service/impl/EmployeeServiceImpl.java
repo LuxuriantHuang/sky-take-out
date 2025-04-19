7f5ca84db440709a5b168f0221470e3863ff6c6d
LuxuriantHuang <cvvvcv@qq.com>
2025-04-19 22:43:59 +0800

    公共字段自动填充功能代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -85,13 +85,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setCreateTime(LocalDateTime.now());
+//        employee.setUpdateTime(LocalDateTime.now());
 
         // 设置当前记录创建人和修改人id
-        Long empid = BaseContext.getCurrentId();
-        employee.setCreateUser(empid);
-        employee.setUpdateUser(empid);
+//        Long empid = BaseContext.getCurrentId();
+//        employee.setCreateUser(empid);
+//        employee.setUpdateUser(empid);
 
         employeeMapper.insert(employee);
     }
@@ -122,8 +122,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = Employee.builder()
                 .status(status)
                 .id(id)
-                .updateTime(LocalDateTime.now())
-                .updateUser(BaseContext.getCurrentId())
+//                .updateTime(LocalDateTime.now())
+//                .updateUser(BaseContext.getCurrentId())
                 .build();
         employeeMapper.update(employee);
     }
