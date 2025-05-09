05eabbd4d4c30e534cb89d07cbe59d02f6cc1d3a
LuxuriantHuang <cvvvcv@qq.com>
2025-05-09 21:28:11 +0800

    订单状态定时处理相关代码

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,53 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Scheduled(cron = "0 * * * * ? ")
+//    @Scheduled(cron = "0/5 * * * * ?")//test
+    public void processTimeoutOrder() {
+        log.info("定时处理超时订单：{}", LocalDateTime.now());
+
+        // select * from orders where status = ? and order_time < (now() - 15)
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, LocalDateTime.now().plusMinutes(-15));
+        if (ordersList != null && !ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 1 * * ? ")
+//    @Scheduled(cron = "1/5 * * * * ?")//test
+    public void processDeliveryOrder() {
+        log.info("定时处理派送中订单：{}", LocalDateTime.now());
+
+        // select * from orders where status = ? and order_time < ?
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, LocalDateTime.now().plusHours(-1));
+        if (ordersList != null && !ordersList.isEmpty()) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.COMPLETED);
+                orders.setDeliveryTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
